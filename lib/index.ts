b362297c6e7cba1791911ba1c17f1fcccba1582e
Synqat <ken@slinky.link>
2024-09-26 15:38:24 -0600

    fix: forgot to import a nice-to-have type

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,8 @@
 import { fastifyPlugin } from 'fastify-plugin'
 import { plugin, type FastifyRouterPluginOptions } from 'lib/plugin'
+import { type FastifyRouterOptions } from 'lib/loadRoutes'
 
-export { type FastifyRouterPluginOptions }
+export { type FastifyRouterPluginOptions, type FastifyRouterOptions }
 export const fastifyRouter = fastifyPlugin(plugin)
 
 export default fastifyRouter
